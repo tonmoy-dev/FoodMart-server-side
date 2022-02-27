14a628f57243cf8541fcdfc46467eed5a2573f70
Anik-Nath <anikdevnath03112000@gmail.com>
2022-02-28 02:01:06 +0600

    added single page backend

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-const express = require('express')
-const { MongoClient } = require('mongodb');
-const app = express()
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ernrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-async function run(){
-
+async function run() {
   try {
     await client.connect();
     console.log("database connected");
-    const database = client.db('foodmart_shop');
-    const productCollection = database.collection('products')
-    const blogCollection = database.collection('blogs')
-    const reviewCollection = database.collection('reviews')
+    const database = client.db("foodmart_shop");
+    const productCollection = database.collection("products");
+    const blogCollection = database.collection("blogs");
+    const reviewCollection = database.collection("reviews");
+    const cartCollection = database.collection("cart");
 
-
-    app.get('/products', async (req, res) => {
-      const cursor = productCollection.find({})
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
       const products = await cursor.toArray();
       res.send(products);
-  })
-    app.get('/blogs', async (req, res) => {
-      const cursor = blogCollection.find({})
+    });
+
+    //single data
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      console.log(id);
+      const oneProduct = await productCollection.findOne(query);
+      res.json(oneProduct);
+    });
+
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogCollection.find({});
       const blogs = await cursor.toArray();
       res.send(blogs);
-  })
-    app.get('/reviews', async (req, res) => {
-      const cursor = reviewCollection.find({})
+    });
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find({});
       const reviews = await cursor.toArray();
       res.send(reviews);
-  })
+    });
 
-  } 
-  finally {
+    app.post("/cart", async (req, res) => {
+      const cartlist = req.body;
+      const result = await cartCollection.insertOne(cartlist);
+      res.send(result);
+    });
+  } finally {
     // await client.close();
   }
-
-
-
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello Foodmart!!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Foodmart!!");
+});
 
 app.listen(port, () => {
-  console.log(` listening at ${port}`)
-})
+  console.log(` listening at ${port}`);
+});
